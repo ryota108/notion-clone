0a15b6fbd8e978ef2e39a6de359cfb9adbf764e5
Ryota <aya.ryo.718@outlook.jp>
2023-01-06 13:09:14 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 5050;
+
+app.get("/",(req,res)=>{
+    res.send("Hello Express")
+})
+
+// ユーザー新規登録API
+
+// ユーザーログイン用API
+
+app.listen(PORT,()=>{
+    console.log("ローカルサーバー起動中...")
+})
