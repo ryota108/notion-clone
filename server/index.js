6356ca712e82bb1d6b55fa46789cf74713ee6c42
Ryota <aya.ryo.718@outlook.jp>
2023-01-06 13:40:46 +0900

    パスワードの保護とDBへの登録完了

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 5050;
 require("dotenv").config();
-
+const CryptoJS = require("crypto-js")
+const User = require("./src/v1/models/user")
 // DB接続
 try {
     mongoose.connect(process.env.MONGODB_URL)
@@ -12,7 +13,18 @@ try {
 }
 
 // ユーザー新規登録API
+app.post("/register",async (req,res)=> {
+    const password = req.body.password
 
+    try {
+    // パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password,process.env.SECRET_KEY);
+    // ユーザーの新規登録
+    const user = await User.create(req.body)
+    }catch {
+
+    }
+})
 // ユーザーログイン用API
 
 app.listen(PORT,()=>{
