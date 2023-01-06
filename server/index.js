1400f5b6d28aed83e8df391bd46cb46ab46d480b
Ryota <aya.ryo.718@outlook.jp>
2023-01-06 13:23:16 +0900

    MongoDBと接続完了

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5050;
+require("dotenv").config();
 
-app.get("/",(req,res)=>{
-    res.send("Hello Express")
-})
+// DB接続
+try {
+    mongoose.connect(process.env.MONGODB_URL)
+} catch {
+    console.log(error)
+}
 
 // ユーザー新規登録API
 
