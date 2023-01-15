6d884a07406b076ed6fe4dc01078b86ea0bc200f
Ryota <aya.ryo.718@outlook.jp>
2023-01-15 23:11:32 +0900

    新規登録フォームのバリデーションチェック

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,47 @@
 import { TextField, Button } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
 import authApi from "../api/authApi";
 
 const Register = () => {
+ 
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmErrText, setConfirmErrText] = useState("");
 
   const submitHandler = async (e) =>{
     e.preventDefault()
+    setUsernameErrText("");
+    setPasswordErrText("");
+    setConfirmErrText("");
+
     const data = new FormData(e.target);
     const username = data.get("username").trim();
     const password = data.get("password").trim();
-    const confirmPassword = data.get("username").trim();
+    const confirmPassword = data.get("confirmPassword").trim();
 
+    let error = false;
+ 
+
+   if(username === "") {
+    error= true
+    setUsernameErrText("名前を入力してください");
+   } if(password === "") {
+    error= true
+    setPasswordErrText("パスワードを入力してください");
+   }
+   if(confirmPassword === "") {
+    error= true
+    setConfirmErrText("確認用パスワードを入力してください");
+   } 
+   if(password !== confirmPassword){
+    error= true
+    setConfirmErrText("パスワードと確認用パスワードが異なります。")
+   }
+
+   if(error) return;
 
     // 新規登録APIを叩く
     try {
@@ -25,13 +53,26 @@ const Register = () => {
       localStorage.setItem("token",res.token)
       console.log("新規登録成功")
     } catch (err){
-     console.log(err)
+     const errors = err.data.errors;
+     console.log(errors)
+
+      errors.forEach((err)=>{
+        if(err.param === "username"){
+          setUsernameErrText(err.msg);
+        }
+        if(err.param === "password"){
+          setPasswordErrText(err.msg);
+        }
+        if(err.param === "confirmPassword"){
+          setConfirmErrText(err.msg);
+        }
+      })
     }
   }
 
   return (
     <>
-    <Box component="form" onSubmit={submitHandler}>
+    <Box component="form" onSubmit={submitHandler} noValidate>
       <TextField
         fullWidth
         id="username"
@@ -39,6 +80,8 @@ const Register = () => {
         margin="normal"
         name="username"
         required
+        helperText={usernameErrText}
+        error={usernameErrText!==""}
       />
       <TextField
         fullWidth
@@ -48,6 +91,8 @@ const Register = () => {
         name="password"
         margin="normal"
         required
+        helperText={passwordErrText}
+        error={passwordErrText!==""}
       />
       <TextField
         fullWidth
@@ -57,6 +102,8 @@ const Register = () => {
         name="confirmPassword"
         margin="normal"
         required
+        helperText={confirmErrText}
+        error={confirmErrText!==""}
       />
       <LoadingButton
         sx={{ mt: 3, mb: 2 }}
