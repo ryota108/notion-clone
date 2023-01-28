007ceb245559b7741a13392844634dc804bfe476
Ryota <aya.ryo.718@outlook.jp>
2023-01-28 23:26:58 +0900

    サイドバー作成

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import { Drawer, List, ListItemButton,Box, Typography, IconButton } from "@mui/material";
+import LogoutOutlinedIcon from "@mui/icons-material/Logout"
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined"
+import assets from "../../assets";
+import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
+  const navigate = useNavigate()
+  const logoutHandler = () =>{
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 250, height: "100vh" }}
+    >
+<List sx={{width:250,height:"100vh",backgroundColor:assets.colors.secondary}}>
+  <ListItemButton>
+    <Box
+    sx={{
+      width:"100%",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"space-between"
+    }}
+    >
+   <Typography variant="body2" fontWeight="700">
+    Ryota
+   </Typography>
+   <IconButton onClick={logoutHandler}>
+    <LogoutOutlinedIcon/>
+   </IconButton>
+    </Box>
+  </ListItemButton>
+  <Box sx={{paddingTop:"10px"}}></Box>
+  <ListItemButton>
+    <Box
+    sx={{
+      width:"100%",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"space-between"
+    }}
+    >
+   <Typography variant="body2" fontWeight="700">
+    お気に入り
+   </Typography>
+   <IconButton>
+   </IconButton>
+    </Box>
+  </ListItemButton>
+  <Box sx={{paddingTop:"10px"}}></Box>
+  <ListItemButton>
+    <Box
+    sx={{
+      width:"100%",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"space-between"
+    }}
+    >
+   <Typography variant="body2" fontWeight="700">
+    プライベート
+   </Typography>
+   <IconButton>
+   <AddBoxOutlinedIcon/>
+   </IconButton>
+    </Box>
+  </ListItemButton>
+</List>
 
-export default Sidebar
+    </Drawer>
+  );
+};
+
+export default Sidebar;
