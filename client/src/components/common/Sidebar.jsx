0fa1e97117f8688da14c2c877f80e5b515cf1604
Ryota <aya.ryo.718@outlook.jp>
2023-02-05 23:41:07 +0900

    メモのデモを作成

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Drawer, List, ListItemButton,Box, Typography, IconButton } from "@mui/material";
-import LogoutOutlinedIcon from "@mui/icons-material/Logout"
-import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined"
+import {
+  Drawer,
+  List,
+  ListItemButton,
+  Box,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import LogoutOutlinedIcon from "@mui/icons-material/Logout";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import assets from "../../assets";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 const Sidebar = () => {
-  const navigate = useNavigate()
-  const user = useSelector((state) => state.user.value );
-  const logoutHandler = () =>{
-    localStorage.removeItem("token")
-    navigate("/login")
-  }
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user.value);
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <Drawer
       container={window.document.body}
@@ -19,61 +26,72 @@ const Sidebar = () => {
       open={true}
       sx={{ width: 250, height: "100vh" }}
     >
-<List sx={{width:250,height:"100vh",backgroundColor:assets.colors.secondary}}>
-  <ListItemButton>
-    <Box
-    sx={{
-      width:"100%",
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"space-between"
-    }}
-    >
-   <Typography variant="body2" fontWeight="700">
-    {user.username}
-   </Typography>
-   <IconButton onClick={logoutHandler}>
-    <LogoutOutlinedIcon/>
-   </IconButton>
-    </Box>
-  </ListItemButton>
-  <Box sx={{paddingTop:"10px"}}></Box>
-  <ListItemButton>
-    <Box
-    sx={{
-      width:"100%",
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"space-between"
-    }}
-    >
-   <Typography variant="body2" fontWeight="700">
-    お気に入り
-   </Typography>
-   <IconButton>
-   </IconButton>
-    </Box>
-  </ListItemButton>
-  <Box sx={{paddingTop:"10px"}}></Box>
-  <ListItemButton>
-    <Box
-    sx={{
-      width:"100%",
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"space-between"
-    }}
-    >
-   <Typography variant="body2" fontWeight="700">
-    プライベート
-   </Typography>
-   <IconButton>
-   <AddBoxOutlinedIcon/>
-   </IconButton>
-    </Box>
-  </ListItemButton>
-</List>
-
+      <List
+        sx={{
+          width: 250,
+          height: "100vh",
+          backgroundColor: assets.colors.secondary,
+        }}
+      >
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              {user.username}
+            </Typography>
+            <IconButton onClick={logoutHandler}>
+              <LogoutOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              お気に入り
+            </Typography>
+            <IconButton></IconButton>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <ListItemButton
+          sx={{ pl: "20px" }}
+          component={Link}
+          to="memo/gaojgoejpgap"
+        >
+          <Typography>📝仮おきのメモ</Typography>
+        </ListItemButton>
+      </List>
     </Drawer>
   );
 };
